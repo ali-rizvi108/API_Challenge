c5a06150cf46b1dad6cea462033bab981990cd12
Ali <60332704+ali-rizvi108@users.noreply.github.com>
2020-06-28 17:11:36 -0400

    Update api_keys.py

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "c25c1fa606e9a7409b8debce862b211a"
+weather_api_key = "xxx"
 
 # Google API Key
-g_key = "AIzaSyB6WMPnkMr6JGi7tP5q9Hda2uOwsoP3NgM"
+g_key = "xxx"
